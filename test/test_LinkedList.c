4c356ab5b1292852079d5f865bfbdd3122fe2424
9drashijin <9drashijin@live.com>
2014-02-27 22:58:57 +0800

    added TEST for remove First ELement
    Signed-off-by: 9drashijin <9drashijin@live.com>

--- test/test_LinkedList.c
+++ test/test_LinkedList.c
@@ -103,3 +103,38 @@ void test_LinkedList_delete_middle_element(){
 //	print(); //to test and c the address and data
 }
 
+void test_LinkedList_delete_first_element(){
+	LinkedList *list = createLinkedList();
+	
+	Element arrayElem[] = {{.next = &arrayElem[1], .data = 1},
+							{.next = &arrayElem[2], .data = 2},
+							{.next = NULL, .data = 3}};
+	list->length = 3;
+	list->head = &arrayElem[0];
+	list->tail = &arrayElem[2];
+	///print(); //to test and c the address and data
+	
+	List_delete(list, &arrayElem[0]);
+	
+	TEST_ASSERT_NOT_NULL(list->head); //head have value
+	TEST_ASSERT_NOT_NULL(list->tail); //tail have value
+
+	TEST_ASSERT_EQUAL(&arrayElem[1],list->head); // list head point to 2nd element
+	TEST_ASSERT_EQUAL(&arrayElem[2],list->tail); // list tail pointing to last element
+	TEST_ASSERT_EQUAL(2, list->length); 		 // the length is 2 after deleted element 1
+	
+	TEST_ASSERT_NOT_NULL(arrayElem[0].next); 	 //next -> 1, should be NOT NULL
+	TEST_ASSERT_NOT_NULL(arrayElem[1].next); 	 //next -> 2, should be NOT NULL
+	TEST_ASSERT_NULL(arrayElem[2].next);	 	 // the pointer next of last element should be NULL
+	
+	TEST_ASSERT_EQUAL(1, arrayElem[0].data); //element 1 , array [0] data should be 1
+	TEST_ASSERT_EQUAL(2, arrayElem[1].data); //element 2 , array [1] data should be 2
+	TEST_ASSERT_EQUAL(3, arrayElem[2].data); //element 3 , array [2] data should be 3
+	
+	TEST_ASSERT_EQUAL_PTR(&arrayElem[1], arrayElem[0].next); //pointer of element 1 pointing to element 2
+	TEST_ASSERT_EQUAL_PTR(&arrayElem[2], arrayElem[1].next); //pointer of element 2 pointing to element 3
+	TEST_ASSERT_EQUAL_PTR(NULL, arrayElem[2].next); 		 //pointer of element 3 pointing to NULL
+
+	//print(); //to test and c the address and data
+}
+
